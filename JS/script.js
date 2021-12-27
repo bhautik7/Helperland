20075a0cb6edebae75b5c4a46c764a927a1c6ac4
bhautik sorathiya <bhautiksorthiya8@gmail.com>
2021-12-27 13:15:52 +0530

    add service history page

--- JS/script.js
+++ JS/script.js
@@ -56,5 +56,23 @@ function scrollToTop() {
 
 // price page end
 
+// upcoming page start
 
-// service provider page
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+
+
+// upcoming page end
